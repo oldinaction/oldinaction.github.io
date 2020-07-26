29cc36971a3eeaca4217f1d92e55e00ab2d58263
smalle <oldinaction@qq.com>
2020-07-27 00:06:38 +0800

    Site updated: 2020-07-27 00:05:23

---
+++ src/schemes/pisces.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){function e(){return n.height()+CONFIG.sidebar.offset}function i(n){return t.css({"margin-top":n})}var n=$(".header-inner"),t=$("#sidebar"),r=window.matchMedia("(min-width: 991px)");i(e()).show(),r.addListener(function(n){n.matches&&i(e())})});
