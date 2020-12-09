915fe0461dd3a5288d6c7a74449cb2e4620521dc
smalle <oldinaction@qq.com>
2020-12-09 16:25:27 +0800

    update blog

--- blog-deploy.sh
+++ blog-deploy.sh
@@ -5,5 +5,5 @@ git add .
 git commit -am "update blog"
 git push origin master:source
 export HEXO_ALGOLIA_INDEXING_KEY=3330f3cbaa099dfc30395de5f5b20151
-hexo g && gulp && hexo algolia && hexo d
+hexo g && gulp && hexo d
 
